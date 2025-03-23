8f983bf90a23f62991830c040468fc682f344a54
Nguyễn Văn Chính <157895647+nguyenchinhh@users.noreply.github.com>
2025-03-23 19:40:50 +0700

    Day11.PRACTICE_MID_COURSE_TEST.sql

--- Day11.PRACTICE_MID_COURSE_TEST.sql
+++ Day11.PRACTICE_MID_COURSE_TEST.sql
@@ -97,13 +97,69 @@ ORDER BY so_luong DESC
 
 
 
+/* Question 6:
+Level: Intermediate
+Topic: LEFT JOIN & FILTERING
+Task: Tìm các địa chỉ không liên quan đến bất kỳ khách hàng nào.
+Question: Có bao nhiêu địa chỉ như vậy?
+Answer: 4 */
+-- SELECT * FROM customer
+-- SELECT * FROM address
+
+SELECT COUNT(*) AS so_luong
+FROM address AS a
+LEFT JOIN customer AS c
+ON c.address_id = a.address_id
+WHERE c.address_id IS NULL
 
 
+/* Question 7:
+Level: Intermediate
+Topic: JOIN & GROUP BY
+Task: Danh sách các thành phố và doanh thu tương ừng trên từng thành phố
+Question: Thành phố nào đạt doanh thu cao nhất ?
+Answer: Cape Coral : 221.55 
+SELECT * FROM city
+SELECT * FROM payment
+SELECT * FROM customer
+SELECT * FROM address */
+
+SELECT c.city,
+	SUM(p.amount) AS total_amount
+FROM payment AS p 
+JOIN customer AS cu
+ON p.customer_id = cu.customer_id
+JOIN address AS a
+ON a.address_id = cu.address_id
+JOIN city AS c
+ON c.city_id = a.city_id
+GROUP BY c.city
+ORDER BY total_amount DESC
 
 
+/* Question 8:
+Level: Intermediate
+Topic: JOIN & GROUP BY
+Task: Tạo danh sách trả ra 2 cột dữ liệu:
+   cột 1: thông tin thành phố và đất nước ( format: “city, country")
+   cột 2: doanh thu tương ứng với cột 1
+Question: Thành phố của đất nước nào đat doanh thu thấp nhất ?
+Answer: United States, Tallahassee : 50.85. */
 
-
-
+SELECT ci.city,
+		co.country,
+		SUM(p.amount) AS total_amount
+FROM payment AS p
+JOIN customer AS cu
+ON p.customer_id = cu.customer_id
+JOIN address AS a
+ON a.address_id = cu.address_id
+JOIN city AS ci
+ON ci.city_id = a.city_id
+JOIN country AS co
+ON co.country_id = ci.country_id 
+GROUP BY 1, 2
+ORDER BY 3
 
 
 
