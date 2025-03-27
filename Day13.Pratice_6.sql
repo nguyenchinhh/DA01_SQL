cccb5e8ebeb51c608e46b7a915d03615a929fabc
Nguyễn Văn Chính <157895647+nguyenchinhh@users.noreply.github.com>
2025-03-27 20:10:38 +0700

    Day13.Pratice_6.sql

--- Day13.Pratice_6.sql
+++ Day13.Pratice_6.sql
@@ -17,6 +17,30 @@ WHERE ranking <= 2
 
 
 -- EX3: datalemur-frequent-callers
+WITH ex1 AS (
+    SELECT policy_holder_id 
+    FROM callers 
+    GROUP BY policy_holder_id
+    HAVING COUNT(DISTINCT case_id) >= 3
+)
+SELECT COUNT(policy_holder_id) AS remember
+FROM ex1
+
+
+-- EX4: datalemur-sql-page-with-no-likes
+SELECT a.page_id
+FROM pages AS a 
+LEFT JOIN page_likes AS b
+ON a.page_id = b.page_id
+WHERE b.page_id IS NULL
+GROUP BY a.page_id
+ORDER BY a.page_id ASC
+
+
+
+
+
+
 
 
 
