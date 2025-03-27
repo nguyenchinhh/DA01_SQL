60ffd282a93573b8dbf4108104f7fe7e9d725528
Nguyễn Văn Chính <157895647+nguyenchinhh@users.noreply.github.com>
2025-03-27 22:07:35 +0700

    Day13.Pratice_6.sql

--- Day13.Pratice_6.sql
+++ Day13.Pratice_6.sql
@@ -59,7 +59,110 @@ FROM Transactions
 GROUP BY 1, 2;
 
 
--- EX7: 
+-- EX7:leetcode - product-sales-analysis
+-- SELECT s.product_id, s.year AS first_year, s.quantity, s.price
+-- FROM product AS p
+-- INNER JOIN sales AS s
+-- ON  s.product_id = p.product_id 
+-- GROUP BY s.product_id
+
+SELECT 
+  product_id, 
+  year AS first_year, 
+  quantity, 
+  price 
+FROM Sales 
+WHERE (product_id, year) IN (
+    SELECT 
+      product_id, 
+      MIN(year) AS year 
+    FROM Sales 
+    GROUP BY  product_id
+);
+
+
+-- EX8: leetcode-customers-who-bought-all-products
+SELECT customer_id
+FROM customer 
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT count(*) from product)
+
+-- EX9: leetcode - employees-whose-manager-left-the-company
+SELECT employee_id
+FROM Employees 
+WHERE salary < 30000 AND manager_id NOT IN(
+    SELECT employee_id FROM Employees
+)
+GROUP BY employee_id
+ORDER BY employee_id ASC
+
+-- EX10: datalemur - duplicate-job-listings 
+SELECT COUNT(*) - COUNT(DISTINCT company_id || description) AS duplicate_companies
+FROM job_listings 
+
+
+-- EX11: leetcode-movie-rating
+-- 1 nửa đầu
+WITH ex AS (
+    SELECT u.name,
+        COUNT(*) AS total
+    FROM users AS u
+    INNER JOIN MovieRating  AS m
+    ON u.user_id = m.user_id      
+    GROUP BY u.name
+)
+
+SELECT name AS results
+FROM ex
+WHERE total = (
+    SELECT MAX(total) FROM ex
+)
+GROUP BY name
+ORDER BY name
+LIMIT 1
+
+
+
+-- đúng
+(
+SELECT name AS results
+FROM users 
+INNER JOIN MovieRating 
+using (user_id)
+GROUP BY user_id
+ORDER BY COUNT(rating) DESC, name
+LIMIT 1
+)
+
+UNION ALL 
+
+(
+SELECT title AS results
+FROM movies AS m
+INNER JOIN MovieRating
+USING (movie_id)
+WHERE MONTH(created_at) = '02' AND YEAR(created_at) = '2020'
+GROUP BY title
+ORDER BY avg(rating) DESC, title 
+LIMIT 1
+
+)
+
+
+-- EX12: leetcode-friend-requests-ii-who-has-the-most-friends
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL 
+    SELECT accepter_id  FROM RequestAccepted
+) AS AllFriends
+GROUP BY id
+ORDER BY num DESC 
+LIMIT 1
+
+
+
+
 
 
 
