0f3d4a4932c752abe574be26957e84094e66fce2
Nguyễn Văn Chính <157895647+nguyenchinhh@users.noreply.github.com>
2025-03-31 10:18:43 +0700

    Day15.sql

--- Day15.sql
+++ Day15.sql
@@ -20,6 +20,41 @@ FROM twt_card
 WHERE stt = 1
 ORDER BY 	issued_amount DESC
 
+-- EX3: datalemur - sql-third-transaction
+-- subquert
+SELECT user_id, spend, transaction_date
+FROM (
+  SELECT user_id, spend, transaction_date,
+  ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY transaction_date) AS total
+  FROM transactions 
+) AS c 
+WHERE c.total = 3
+
+
+-- CTE
+WITH twt_user AS (
+SELECT user_id, spend, transaction_date,
+      ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY transaction_date) AS total
+FROM transactions 
+) 
+SELECT user_id, spend, transaction_date
+FROM twt_user 
+WHERE total = 3
+
+
+-- EX4: datalumer - histogram-users-purchases
+WITH ex1 AS (
+SELECT transaction_date, user_id, 
+      product_id,
+      RANK() OVER(PARTITION BY user_id  ORDER BY transaction_date DESC) AS rank1
+FROM user_transactions 
+)
+SELECT transaction_date, user_id, 
+      COUNT(product_id) AS purcharse_id
+FROM ex1
+WHERE rank1 = 1
+GROUP BY 1, 2
+ORDER BY 1
 
 
 
