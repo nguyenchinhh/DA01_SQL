9c4587d146bea65aca998d5c117a9a1f33d694e6
Nguyễn Văn Chính <157895647+nguyenchinhh@users.noreply.github.com>
2025-03-31 09:47:13 +0700

    Day15.sql

--- Day15.sql
+++ Day15.sql
@@ -7,7 +7,18 @@ SELECT EXTRACT(YEAR FROM transaction_date) AS year,
 FROM user_transactions 
 
 
+-- EX2: datalemur -card-launch-success
 
+WITH twt_card AS (
+    SELECT card_name, issued_amount,
+        RANK() OVER(PARTITION BY card_name ORDER BY issue_year, issue_month) AS stt
+    FROM monthly_cards_issued 
+
+)
+SELECT card_name, issued_amount
+FROM twt_card 
+WHERE stt = 1
+ORDER BY 	issued_amount DESC
 
 
 
