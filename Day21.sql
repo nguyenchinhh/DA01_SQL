e5ebdc88da6750cfbf482bc1f82d0bf9439a7789
Nguyễn Văn Chính <157895647+nguyenchinhh@users.noreply.github.com>
2025-04-04 10:29:43 +0700

    Update Day21.sql

--- Day21.sql
+++ Day21.sql
@@ -17,8 +17,6 @@ ORDER BY 1
 
 
 
-
-
 -- 2.  Giá trị đơn hàng trung bình (AOV) và số lượng khách hàng mỗi tháng
 /*
 Thống kê giá trị đơn hàng trung bình và tổng số người dùng khác nhau mỗi tháng 
@@ -46,7 +44,6 @@ tìm các KH tuổi trẻ nhất và gán tag ‘youngest’  
 tìm các KH tuổi trẻ nhất và gán tag ‘oldest’ 
 Insight là gì? (Trẻ nhất là bao nhiêu tuổi, số lượng bao nhiêu? Lớn nhất là bao nhiêu tuổi, số lượng bao nhiêu) 
 */
-
 WITH chinh AS (
 select first_name, 
       last_name,
@@ -87,69 +84,6 @@ FROM chinh
 GROUP BY 1
 
 
--- 1. Số lượng đơn hàng và số lượng khách hàng mỗi tháng
--- select FORMAT_TIMESTAMP('%Y-%m',created_at) AS month_year,
---       COUNT(user_id) AS total_user,
---       COUNT(order_id) AS total_order
--- from bigquery-public-data.thelook_ecommerce.orders
--- WHERE created_at >= '2019-01-01' AND created_at <= '2022-04-01' AND status = 'Complete'
--- GROUP BY 1
--- ORDER BY 1
-
--- 2.  Giá trị đơn hàng trung bình (AOV) và số lượng khách hàng mỗi tháng
--- select format_timestamp('%Y-%m',created_at) AS month_year,
---       COUNT(DISTINCT user_id) AS distinct_users,
---       ROUND(SUM(sale_price) / COUNT(order_id),2)  AS average_order_value 
--- from bigquery-public-data.thelook_ecommerce.order_items
--- where created_at >= '2019-01-01' AND created_at <= '2022-04-01'
--- GROUP BY 1
--- ORDER BY 1
-
-
-
-
-
--- WITH chinh AS (
--- select first_name, 
---       last_name,
---       gender,
---       age,
---       'youngest' AS tag
--- from bigquery-public-data.thelook_ecommerce.users
--- where created_at >= '2019-01-01' AND created_at <= '2022-04-30'
---       AND (age) IN (
---         SELECT 
---         MIN(age) AS age
---         FROM bigquery-public-data.thelook_ecommerce.users
---         WHERE created_at >= '2019-01-01' AND created_at <= '2022-04-30'
---         GROUP BY gender
---       )
--- UNION ALL
--- select first_name, 
---       last_name,
---       gender,
---       age,
---       'oldest' AS tag
--- from bigquery-public-data.thelook_ecommerce.users
--- where created_at >= '2019-01-01' AND created_at <= '2022-04-30'
---       AND (age) IN(
---         SELECT 
---         MAX(age) AS age
---         FROM bigquery-public-data.thelook_ecommerce.users
---         WHERE created_at >= '2019-01-01' AND created_at <= '2022-04-30'
---         GROUP BY gender
---       )
--- )
-
--- SELECT tag,
---       MIN(age) AS min_age,
---       MAX(age) AS max_age,
---       COUNT(*) AS total_users
--- FROM chinh
--- GROUP BY 1
-
-
-
 
 /* 4.Top 5 sản phẩm mỗi tháng.
 Thống kê top 5 sản phẩm có lợi nhuận cao nhất từng tháng (xếp hạng cho từng sản phẩm). 
@@ -171,118 +105,14 @@ join bigquery-public-data.thelook_ecommerce.order_items as b
 on a.product_id = b.product_id
 LIMIT 5
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* 5.Doanh thu tính đến thời điểm hiện tại trên mỗi danh mục
+Thống kê tổng doanh thu theo ngày của từng danh mục sản phẩm (category) trong 3 tháng qua ( giả sử ngày hiện tại là 15/4/2022)
+Output: dates (yyyy-mm-dd), product_categories, revenue */
+select date(created_at) AS dates,
+       product_category,
+       SUM(product_retail_price - cost) AS revenue
+from bigquery-public-data.thelook_ecommerce.inventory_items
+where created_at <= '2022-04-15'
+GROUP BY 1, 2
+ORDER BY dates
 
