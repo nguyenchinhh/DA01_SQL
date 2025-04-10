5c5e4e4d22648d1f2fcb8fbca695d90b6395183a
Nguyễn Văn Chính <157895647+nguyenchinhh@users.noreply.github.com>
2025-04-10 08:31:15 +0700

    Update Day22.sql

--- Day22.sql
+++ Day22.sql
@@ -32,50 +32,86 @@ order by year_month )
 
 
 
+ -- 2.Tạo retention cohort analysis.
+
+WITH online_retail_convert AS (
+select invoiceno,
+stockcode,
+description,
+CAST(quantity AS int) as quantity,
+CAST(invoicedate AS timestamp) as invoicedate,
+CAST(unitpeice AS numeric) as unitpeice,
+customerid,
+country
+from online_retail
+where customerid <> ''
+and CAST(quantity AS int) > 0
+and CAST(unitpeice AS numeric) > 0)
+
+, online_retail_main AS (
+select * from (
+select *,
+ROW_NUMBER() OVER(PARTITION BY invoiceno, stockcode, quantity ORDER BY invoicedate) as stt
+from online_retail_convert) as t
+where stt = 1 )
+
+
+, online_retail_index AS (
+select 
+customerid,
+amount,
+TO_CHAR(first_purchase_date, 'yyyy-mm') as cohort_date,
+invoicedate,
+(extract('year' from invoicedate) - extract('year' from first_purchase_date)) * 12
++ (extract('month' from invoicedate) - extract('month' from first_purchase_date)) + 1 as index
+
+from (
+select 
+customerid,
+unitpeice * quantity as amount,
+MIN(invoicedate) OVER(PARTITION BY customerid) as first_purchase_date,
+invoicedate
+from online_retail_main) as a)
+
+, xxx AS (
+select 
+cohort_date,
+index,
+COUNT(DISTINCT customerid) as cnt,
+SUM(amount) as revenue
+from online_retail_index
+group by cohort_date, index)
+
+/*Bước 3:
+pivot table ==> cohort chart
+*/
+
+-- customer cohort
+,customer_cohort AS (
+select 
+cohort_date,
+SUM(case when index = 1 then cnt else 0 end) as "m1",
+SUM(case when index = 2 then cnt else 0 end) as "m2",
+SUM(case when index = 3 then cnt else 0 end) as "m3",
+SUM(case when index = 4 then cnt else 0 end) as "m4"
+from xxx
+GROUP BY cohort_date
+ORDER BY cohort_date)
+
+-- retention cohort
+select cohort_date,
+round(100.00 * m1/m1,2) || '%' as m1,
+round(100.00 * m2/m1,2) || '%' as m2,
+round(100.00 * m3/m1,2) || '%' as m3,
+round(100.00 * m4/m1,2) || '%' as m4
+from customer_cohort
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       
 
 select * from bigquery-public-data.thelook_ecommerce.order_items
 select * from bigquery-public-data.thelook_ecommerce.orders
 select * from bigquery-public-data.thelook_ecommerce.products
-
 select * from bigquery-public-data.thelook_ecommerce.users
-
-
-
-
-
-
-
-
 
 
